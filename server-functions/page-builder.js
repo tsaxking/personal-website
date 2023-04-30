641ded03ea0f85b6acf88b510af4f10e695971d7
tsaxking <81051058+tsaxking@users.noreply.github.com>
2023-04-30 02:53:03 -0600

    changes before switching templates

--- server-functions/page-builder.js
+++ server-functions/page-builder.js
@@ -4,64 +4,64 @@ const { getJSON, getTemplate } = require('./get-file');
 const { render } = require('node-html-constructor').v3;
 
 const builder = {
-        'Compositions': () => {
-            const compositions = getJSON('/compositions');
+    'Compositions': () => {
+        const compositions = getJSON('/compositions');
 
-            const cstr1Opts = {};
-            const cstr2Opts = {};
+        const cstr1Opts = {};
+        const cstr2Opts = {};
 
-            cstr1Opts.compositionTypes = Object.keys(compositions).map((type, i) => {
-                cstr2Opts['compositions-' + i] = compositions[type].map(c => {
-                    if (!c.display) return;
-                    return {
-                        ...c,
-                        compositionTitle: c.title,
-                        disableSample: c.sampleAvailable ? '' : 'disabled'
-                    }
-                }).filter(c => c);
+        cstr1Opts.compositionTypes = Object.keys(compositions).map((type, i) => {
+            cstr2Opts['compositions-' + i] = compositions[type].map(c => {
+                if (!c.display) return;
                 return {
-                    compositionType: type,
-                    pos: i
+                    ...c,
+                    compositionTitle: c.title,
+                    disableSample: c.sampleAvailable ? '' : 'disabled'
                 }
-            });
+            }).filter(c => c);
+            return {
+                compositionType: type,
+                pos: i
+            }
+        });
 
-            return render(render(getTemplate('/compositions'), cstr1Opts), cstr2Opts);
-        },
-        "About Me": () => {
-                const html = getTemplate('/about/about-me');
-                const about = getJSON('/about-me');
+        return render(render(getTemplate('/compositions'), cstr1Opts), cstr2Opts);
+    },
+    "About Me": () => {
+            const html = getTemplate('/about/about-me');
+            const about = getJSON('/about-me');
 
-                const generateLink = (link, text) => {
-                    return `<a href="${link}" ${link.includes('mailto') ? '' : 'target="blank"'}>${text}</a>`;
-                }
+            const generateLink = (link, text) => {
+                return `<a href="${link}" ${link.includes('mailto') ? '' : 'target="blank"'}>${text}</a>`;
+            }
 
-                const generateList = (list) => {
-                        return `
-                        <ul>
-                            ${list.map(item => `<li>${generateSection(item)}</li>`).join('')}
-                        </ul>
-                    `;
-                }
-
-                const generateSection = (section) => {
-                    const {list, link, text, description } = section;
+            const generateList = (list) => {
                     return `
-                        ${link ? generateLink(link.url, link.text) : text} ${description ? '| ' + description : ''}
-                        ${list ? generateList(list) : ''}
-                    `;
-                }
+                    <ul>
+                        ${list.map(item => `<li>${generateSection(item)}</li>`).join('')}
+                    </ul>
+                `;
+            }
 
-                const cstrOpts = {
-                    aboutAccordion: Object.keys(about).map((key) => {
-                        return {
-                            accordionHeading: key,
-                            accordionContent: generateList(about[key]),
-                            accordionLowercaseHeading: key.toLowerCase().replace(new RegExp(' ', 'g'), '-')
-                        }
-                    })
-                };
+            const generateSection = (section) => {
+                const {list, link, text, description } = section;
+                return `
+                    ${link ? generateLink(link.url, link.text) : text} ${description ? '| ' + description : ''}
+                    ${list ? generateList(list) : ''}
+                `;
+            }
 
-                return render(html, cstrOpts);
+            const cstrOpts = {
+                aboutAccordion: Object.keys(about).map((key) => {
+                    return {
+                        accordionHeading: key,
+                        accordionContent: generateList(about[key]),
+                        accordionLowercaseHeading: key.toLowerCase().replace(new RegExp(' ', 'g'), '-')
+                    }
+                })
+            };
+
+            return render(html, cstrOpts);
     },
     'Portfolio': () => {
         const html = getTemplate('/about/portfolio');
